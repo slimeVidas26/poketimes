3f2ee1fb91da988c65a4b2ba1040f3cdb0963566
SlimeVidas <dahan.isaac@gmail.com>
2019-01-31 16:47:17 +0200

    finish router:25

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import {BrowserRouter , Route} from 'react-router-dom'
+
 
 class App extends Component {
   render() {
     return (
+      <BrowserRouter>
       <div className="App">
        <Navbar />
-         
+       <Route exact path = "/" component = {Home}/>
+       <Route path = "/about" component = {About}/>
+       <Route path = "/contact" component = {Contact}/>
       </div>
+      </BrowserRouter>
     );
   }
 }
